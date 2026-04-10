c052603bc844506b087c22073ce6b636e1804dad
westras <r.westholm@telia.com>
2026-04-10 11:35:49 +0200

    started on the js for NAMES textarea

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LandingPageOFDOOM</title>
     <link rel="stylesheet" href="home.css" class="css">
+    <link rel="stylesheet" href="home.js" class="js">
 </head>
 <body>
     
