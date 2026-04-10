1d9b29f478579ef0a83ef214ae11b3cdb67a61ae
westras <r.westholm@telia.com>
2026-04-10 10:12:45 +0200

    skeleton html

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LandingPageOFDOOM</title>
+    
+</head>
+<body>
+    
+    <section class="navbar">
+
+        <div class="titel">
+            <img src="Pictures/GROUPGENERATORRRRR" alt="">
+        </div>
+
+        <div class="nav-links">
+            <a href="#">Home</a>
+        </div>
+
+    </section>
+
+    <section class="main">
+        
+        <div class="topmain">
+        
+            <textarea placeholder="Names..." name="names" id="NAMES">
+
+            </textarea>
+
+            <div class="enteredNames">
+                
+            </div>
+
+        </div>
+
+        <div class="botmain">
+
+            <button> </button>
+
+        </div>
+
+    </section>
+
+
+</body>
+</html>
